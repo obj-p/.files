dd9cee76cc8021ee66187a61a3ca00996a02779d
Jason Prasad <jasongprasad@gmail.com>
2026-06-13 12:31:50 -0400

    Add caddie.lua

---
+++ nvim/lua/plugins/caddie.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"obj-p/caddie.nvim",
+		dir = "~/Projects/caddie.nvim",
+		lazy = false,
+		keys = {
+			{ "<leader>cs", "<cmd>CaddieStart<cr>", desc = "Caddie start recording" },
+			{ "<leader>cq", "<cmd>CaddieStop<cr>", desc = "Caddie stop recording" },
+			{ "<leader>cr", "<cmd>CaddieReview<cr>", desc = "Caddie review session" },
+		},
+		opts = {
+			agent = {
+				provider = "claude-code",
+			},
+		},
+	},
+}
